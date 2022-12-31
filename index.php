d39cf52ea1d9045a62312f29d92181ab024b877e
Javier Reyes <javier.reys@gmail.com>
2022-12-31 01:19:41 -0300

    feat: PDO initial config for pgsql

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+//require 'router.php';
+
+$dsn = "pgsql:host=localhost;port=5432;dbname=restaurant;options='--client_encoding=utf8'";
+
+$pdo = new PDO($dsn, 'postgres', 'changeme');
+
+$statement = $pdo->prepare('select * from products');
+$statement->execute();
+
+$products = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($products as $product) {
+    echo "<li>" . $product['name'] . '</li>';
+}
