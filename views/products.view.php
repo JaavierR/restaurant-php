0848d0f6a205a8a03aefd65a41dc389c249c64b8
Javier Reyes <javier.reys@gmail.com>
2023-01-04 12:21:28 -0300

    refactor: use a table instead of a ul to render the products

--- views/products.view.php
+++ views/products.view.php
@@ -2,19 +2,54 @@
 <?php require 'partials/nav.php' ?>
 <?php require 'partials/header.php' ?>
 
-    <main>
-        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            <ul class="flex flex-col gap-4">
-                <?php foreach ($products as $product): ?>
-                    <li class="bg-white rounded-2xl border border-zinc-500/20 shadow-sm p-4">
-                        <a href="/producto?id=<?= $product['id'] ?>" class="text-blue-500 hover:underline">
-                            <?= $product['name'] ?>
-                        </a>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
+    <main class="mx-auto max-w-7xl py-4">
+        <div class="px-4 sm:px-6 lg:px-8">
+            <div class="sm:flex sm:items-center">
+                <div class="sm:flex-auto">
+                    <h1 class="text-xl font-semibold text-gray-900">Productos</h1>
+                    <p class="mt-2 text-sm text-gray-700">Una lista de todos los productos que se ofrecen en el
+                        menu.</p>
+                </div>
+                <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+                    <button type="button"
+                            class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">
+                        Add product
+                    </button>
+                </div>
+            </div>
+            <div class="mt-8 flex flex-col">
+                <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                        <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+                            <table class="min-w-full divide-y divide-gray-300">
+                                <thead class="bg-gray-50">
+                                <tr>
+                                    <th scope="col"
+                                        class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                                        Name
+                                    </th>
+                                    <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                                        <span class="sr-only">Editar</span>
+                                    </th>
+                                </tr>
+                                </thead>
+                                <tbody class="divide-y divide-gray-200 bg-white">
+                                <?php foreach ($products as $product) : ?>
+                                    <tr>
+                                        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6"><?= $product['name'] ?></td>
+                                        <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                                            <a href="/producto?id=<?= $product['id'] ?>"
+                                               class="text-indigo-600 hover:text-indigo-900">Editar<span class="sr-only">, <?= $product['name'] ?></span></a>
+                                        </td>
+                                    </tr>
+                                <?php endforeach; ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </main>
 
-<?php
-require 'partials/footer.php' ?>
+<?php require 'partials/footer.php' ?>
