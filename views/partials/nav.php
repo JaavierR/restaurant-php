cbb575a5f7c1821fe1f659f02e889e0650c25670
Javier Reyes <javier.reys@gmail.com>
2023-01-04 12:21:02 -0300

    refactor: hidde profile menu

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -79,7 +79,7 @@
                             From: "transform opacity-100 scale-100"
                             To: "transform opacity-0 scale-95"
                         -->
-                        <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                        <div class="hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                              role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button"
                              tabindex="-1">
                             <!-- Active: "bg-zinc-100", Not Active: "" -->
